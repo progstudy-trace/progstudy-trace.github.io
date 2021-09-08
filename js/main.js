612a8eb42ebca461d41bd8621ad47d9e8648dd40
progstudy-trace <90305643+progstudy-trace@users.noreply.github.com>
2021-09-08 15:28:41 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// スクロールしたら下からふわっと
+$(function(){
+  $(window).scroll(function (){
+      $('.fadein').each(function(){
+          var targetElement = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > targetElement - windowHeight + 200){
+              $(this).css('opacity','1');
+              $(this).css('transform','translateY(0)');
+          }
+      });
+  });
+});
+
+
+
